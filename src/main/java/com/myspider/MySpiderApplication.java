018bb07277563dca1a999ae49f6206f5e49b3c8c
coffeezhu007 <coffeezhu007@163.com>
2019-10-24 08:02:30 +0800

    上传springboot必要的框架文件

---
+++ src/main/java/com/myspider/MySpiderApplication.java
@@ -0,0 +1,23 @@
+package com.myspider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableCaching
+@ComponentScan(value = {"com.myspider.component", "com.myspider"})
+@EntityScan(basePackages = "com.myspider.entity")
+@EnableJpaRepositories(basePackages = "com.myspider.repository")
+@EnableFeignClients
+@EnableTransactionManagement
+public class MySpiderApplication {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(MySpiderApplication.class);
+        application.run(args);
+    }
+}
