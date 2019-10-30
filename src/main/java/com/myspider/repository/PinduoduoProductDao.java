ed3c8d5fba5d7ae17f6311b13fbd1cb485b8cb80
coffeezhu007 <coffeezhu007@163.com>
2019-10-30 22:07:38 +0800

    准备把拼多多的数据和采集淘宝的存入通过文件上传的方式存入数据库中

--- src/main/java/com/myspider/repository/PinduoduoProductDao.java
+++ src/main/java/com/myspider/repository/PinduoduoProductDao.java
@@ -11,9 +11,10 @@ import java.util.List;
 @Repository
 public interface PinduoduoProductDao extends JpaRepository<PinduoduoProductsUrlEntity,Long> {
 
+    @Modifying
     @Query(value = "UPDATE T_PDD_GOODS_URL SET STATUS = ?1 WHERE PRODUCT_URL = ?2", nativeQuery = true)
     int updatePinduoduoProductUrlStatus(int status,String productUrl);
 
-    @Modifying
+
     List<PinduoduoProductsUrlEntity> findByStatus(int status);
 }
