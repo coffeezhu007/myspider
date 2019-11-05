95929ca85c049a6f411179f78187f34baa530a59
coffeezhu007 <coffeezhu007@163.com>
2019-11-05 08:01:39 +0800

    11月5号早上开发了拼多多的店铺采集。

--- src/main/java/com/myspider/controller/SpiderPddStoreController.java
+++ src/main/java/com/myspider/controller/SpiderPddStoreController.java
@@ -1,9 +1,14 @@
 package com.myspider.controller;
 
+import com.myspider.dto.request.SpiderPddStoreForm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -16,4 +21,13 @@ public class SpiderPddStoreController {
     }
 
 
+    @RequestMapping(value="/gospider_pdd_store",method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String,Object> spiderPddStore(SpiderPddStoreForm pddStoreForm){
+
+        Map<String,Object> resultMap = new HashMap<>();
+        resultMap.put("status",false);
+
+        return resultMap;
+    }
 }
