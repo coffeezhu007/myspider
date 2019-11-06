6f5051ac9593b032c2544f1bd967fc5070c4ece3
coffeezhu007 <coffeezhu007@163.com>
2019-11-06 22:13:38 +0800

    写了生成token的功能

--- src/main/java/com/myspider/service/impl/SpiderTokenServiceImpl.java
+++ src/main/java/com/myspider/service/impl/SpiderTokenServiceImpl.java
@@ -21,14 +21,9 @@ public class SpiderTokenServiceImpl implements SpiderTokenService {
     /**
      * 字符串池
      */
-    /*private static String[] STR_ARR = new String[] { "a", "b", "c", "d", "e",
+    private static String[] STR_ARR = new String[] { "a", "b", "c", "d", "e",
             "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
             "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E",
-            "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-            "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5",
-            "6", "7", "8", "9", "0" };*/
-
-    private static String[] STR_ARR = new String[] {"A", "B", "C", "D", "E",
             "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
             "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5",
             "6", "7", "8", "9", "0" };
@@ -62,7 +57,13 @@ public class SpiderTokenServiceImpl implements SpiderTokenService {
         StringBuilder sb = new StringBuilder();
         Random rand = new Random();
         for (int i = 0; i < length; i++) {
-            sb.append(STR_ARR[rand.nextInt(STR_ARR.length)]);
+
+            if(i<=53){
+                sb.append(  STR_ARR[rand.nextInt(STR_ARR.length)].toUpperCase() );
+            }
+            else{
+                sb.append(  STR_ARR[rand.nextInt(STR_ARR.length)] );
+            }
         }
         return sb.toString();
     }
