c27bc3424b0e2c6934f57056cd6555ed7b9cfede
coffeezhu007 <coffeezhu007@163.com>
2019-11-06 22:03:37 +0800

    写了生成token的功能

--- src/main/java/com/myspider/service/impl/SpiderTokenServiceImpl.java
+++ src/main/java/com/myspider/service/impl/SpiderTokenServiceImpl.java
@@ -21,9 +21,14 @@ public class SpiderTokenServiceImpl implements SpiderTokenService {
     /**
      * 字符串池
      */
-    private static String[] STR_ARR = new String[] { "a", "b", "c", "d", "e",
+    /*private static String[] STR_ARR = new String[] { "a", "b", "c", "d", "e",
             "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
             "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E",
+            "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+            "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "0" };*/
+
+    private static String[] STR_ARR = new String[] {"A", "B", "C", "D", "E",
             "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
             "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5",
             "6", "7", "8", "9", "0" };
